e36fe6db5f775cd72e544a813c91642772e10692
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-29 15:58:51 -0700

    inserting documents into collections

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -11,4 +11,23 @@ export default async (req, res) => {
         .toArray();
     
     res.json(movies);
+    
+    // let newFood = { name: "Cake", calories: 250 };
+
+    // db.collection("food").insertOne(newFood, function (err, res) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log("Added Successfully");
+    //     }
+    // });
+
+    // const foods = await db
+    //     .collection("food")
+    //     .find({})
+    //     .sort({ calories: -1 })
+    //     .limit(20)
+    //     .toArray();
+    
+    // res.json(foods);
 }
